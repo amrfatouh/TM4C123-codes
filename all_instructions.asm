043bc71e25ea3d4c4059314bc0681bc8dcf1896d
Amr Fatouh <amrfatouh1499@gmail.com>
2021-06-22 22:34:18 +0200

    small modifications

--- all_instructions.asm
+++ all_instructions.asm
@@ -1,13 +1,13 @@
 ADD R0, R1, R2
 ADD R0, R1, #2
 ADD R0, R1 ;add R0 and R1 and store the result in R0
-ADC ;?? (doctor's slides)
-//add with carry, add as normal then add the result with the carry flag then store in destination reg
+ADC ; (doctor's slides)
+;add with carry, add as normal then add the result with the carry flag then store in destination reg
 SUB R0, R0, R1
 CMP	
-SBC ;?? (doctor's slides)
-//subtreact with carry, sub as normal then if the carry flag was 0, decrement the result by 1 then store it
-//else if carry flag = 1, store the result as it is.
+SBC ; (doctor's slides)
+;subtreact with carry, sub as normal then if the carry flag was 0, decrement the result by 1 then store it
+;else if carry flag = 1, store the result as it is.
 MUL R0, R0, R1
 DIVU R0, R1 ;R0 / R1
 
@@ -25,9 +25,10 @@ LSL R0, R0, #2
 LSL R0, #2
 LSR R0, R0, #2
 ASR R0, R0, #2
-ROR ;??
-//Rotate Right, like shift right but the elements are rotated and entered again from right
-//Not discarded as in right shift
+ROR ;
+;Rotate Right, like shift right but the elements are rotated and entered again from right
+ROX
+;Rotate Right Extended, like shift right, but the element is kept in the carry then pushed to the most significat bit at the next step
 
 LDR R0, [R1] 			;load(R1) (take address stored in R1 and load the value from the memory into R0)
 LDR R0, [R1, #4]	;load(R1 + 4)
